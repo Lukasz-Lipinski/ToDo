db66a9266352bef7e1e2386b737434a93dc63dc6
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2021-12-17 19:33:17 +0100

    differences solved

--- src/components/Snackbar/redux.js
+++ src/components/Snackbar/redux.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const IS_ADDED_TRUE = "snackbar/elementAdded_true";
 const IS_ADDED_FALSE = "snackbar/elementAdded_false";
 const IS_DONE_TRUE = "snackbar/elementDone_true";
@@ -15,16 +14,16 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-  switch(action.type) {
-    case IS_ADDED_TRUE: 
-			return {
+  switch (action.type) {
+    case IS_ADDED_TRUE:
+      return {
         isAdded: true,
-			};
+      };
     case IS_ADDED_FALSE:
       return {
         isAdded: false,
       };
-    case IS_DONE_TRUE: 
+    case IS_DONE_TRUE:
       return {
         isDone: true
       };
@@ -34,43 +33,5 @@ export default (state = initialState, action) => {
       };
     default:
       return state;
-    }
-=======
-const IS_ADDED_TRUE = "snackbar/elementAdded_true";
-const IS_ADDED_FALSE = "snackbar/elementAdded_false";
-const IS_DONE_TRUE = "snackbar/elementDone_true";
-const IS_DONE_FALSE = "snackbar/elementDone_false";
-
-export const isAddedTrue = () => ({ type: IS_ADDED_TRUE });
-export const isAddedFalse = () => ({ type: IS_ADDED_FALSE });
-export const isDoneTrue = () => ({ type: IS_DONE_TRUE });
-export const isDoneFalse = () => ({ type: IS_DONE_FALSE });
-
-const initialState = {
-  isAdded: false,
-  isDone: false,
-};
-
-export default (state = initialState, action) => {
-  switch(action.type) {
-    case IS_ADDED_TRUE: 
-			return {
-        isAdded: true,
-			};
-    case IS_ADDED_FALSE:
-      return {
-        isAdded: false,
-      };
-    case IS_DONE_TRUE: 
-      return {
-        isDone: true
-      };
-    case IS_DONE_FALSE:
-      return {
-        isDone: false
-      };
-    default:
-      return state;
-    }
->>>>>>> 9f9f7ecf5291bf3006d4b273cdc17fdc114973ae
+  }
 }
